66d79c5fba0c86847759fced597a5b6d6e7f1d3c
Kyuu <Public.kyuuanago@gmail.com>
2022-09-02 19:16:27 +0900

    [update] bashとzshの設定ファイルを分離

--- unlink.sh
+++ unlink.sh
@@ -5,6 +5,9 @@
 # Last Modified Date: 2022 05/04
 # Last Modified By  : nattoujam <Public.Kyuuanago@gmail.com>
 
+cd `dirname $0`
+path=`pwd`
+
 unlink_if_needs () {
   if [ -e $1 ]
   then
@@ -19,15 +22,21 @@ unlink_if_needs () {
   fi
 }
 
-echo uninstall dot files
+echo 'unlink dot files'
 unlink_if_needs ~/.aliases 0
-unlink_if_needs ~/.bash_profile 0
 unlink_if_needs ~/.inputrc 0
 unlink_if_needs ~/.latexmkrc 0
 unlink_if_needs ~/.tmux.conf 0
 unlink_if_needs ~/.wsl1 0
 unlink_if_needs ~/.wsl2 0
 
-echo uninstall my commands
+echo 'unlink bash files'
+sh $path/bash/unlink.sh
+
+echo 'unlink zsh files'
+sh $path/zsh/unlink.sh
+
+echo 'unlink my commands'
 unlink_if_needs /usr/local/bin/ruck 1
 unlink_if_needs /usr/local/bin/git-init-config 1
+unlink_if_needs /usr/local/bin/git-local-nattoujam 1
---
+++ zsh/unlink.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# File              : unlink.sh
+# Date              : 2022 09/02
+# Last Modified Date: 2022 09/02
+
+unlink_if_needs () {
+  if [ -e $1 ]
+  then
+    if [ $2 -eq 1 ]
+    then
+      echo "sudo unlink: $1"
+      sudo unlink $1
+    else
+      echo "unlink: $1"
+      unlink $1
+    fi
+  fi
+}
+
+unlink_if_needs ~/.zshrc 0
